7d1bfcdfdaf3029b2ed9ce576af9f71fa10c79e9
zemola <azeemolatunji@gmail.com>
2022-07-14 22:39:40 +0100

    improved the nav

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,6 +1,7 @@
+import '../styles/Navigation.scss'
 const Navigation = () => {
     return ( 
-        <div>
+        <div className='navbar'>
             <a href="/">Home</a>
             <a href="/">Create</a>
         </div>
