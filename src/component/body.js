d7499f4b005ce0b5fd6611ad0bbe66baaf5ef8de
zemola <azeemolatunji@gmail.com>
2022-07-16 22:41:28 +0100

    handle error

--- src/component/body.js
+++ src/component/body.js
@@ -3,20 +3,27 @@ import '../styles/body.css'
 import BlogList from "./blogList";
 
 const Body = () => {
- const [blogs, setBlogs] = useState([
-{    title: 'My new website', main: "testinggsdkhdbdh sdjsjssjssjs samsdkjssdksd sdsdjssdmksdsdmwsd,wdwddwdknw wdjw wdikcwcwdkwijdwcwkjdlwc wkwjwwwjwjwdwd wdwkw w j wdjw jwd wd wdjwdc wdlnwd cw., wd w wd wd wdc w kkjsdfc ,dfc wdwwwcd", author: 'mario', id: 1 },
-{    title: 'welcome party!!', main: "testinggsdkhdbdh sdjsjssjssjs samsdkjssdksd sdsdjssdmksdsdmwsd,wdwddwdknw wdjw wdikcwcwdkwijdwcwkjdlwc wkwjwwwjwjwdwd wdwkw w j wdjw jwd wd wdjwdc wdlnwd cw., wd w wd wd wdc w kkjsdfc ,dfc wdwwwcd", author: 'Ade', id: 2 },
-{    title: 'web dev top tios', main: "testinggsdkhdbdh sdjsjssjssjs samsdkjssdksd sdsdjssdmksdsdmwsd,wdwddwdknw wdjw wdikcwcwdkwijdwcwkjdlwc wkwjwwwjwjwdwd wdwkw w j wdjw jwd wd wdjwdc wdlnwd cw., wd w wd wd wdc w kkjsdfc ,dfc wdwwwcd", author: 'taiwo', id: 3 }, ])
+ const [blogs, setBlogs] = useState([]);
+ const [isPending, setIsPending] = useState(true)
 
     const handleDelete = (id) => {
-        console.log(id)
         const newBlogs =  blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
 
+    useEffect(()=>{
+      fetch('http://localhost:8000/blogs').then(res => {
+        return res.json();
+      }).then(data =>{
+        setBlogs(data);
+        setIsPending(false)
+      })
+    }, []);
+
   return (
     <div className="body">
-        <BlogList blogs={blogs} handleDelete = {handleDelete} />
+       {isPending && <div>loading.....</div>}
+       {blogs && <BlogList blogs={blogs} handleDelete = {handleDelete} />}
     </div>
   );
 };
